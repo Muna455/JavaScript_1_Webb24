62f6161c3e69d7ce9238b5f3dd26d2948945e131
Muna455 <104700918+Muna455@users.noreply.github.com>
2024-04-12 12:29:20 +0200

    muna

--- 02_JS_var_flode_DOM/Muna/Script.js
+++ 02_JS_var_flode_DOM/Muna/Script.js
@@ -1,2 +1,16 @@
-let Addittion = 2+2;
-console.log (Addittion);
+/*
+let x=2+2
+
+console.log(x)
+
+*/
+
+
+let mynamn = "muna"
+let myage= 25
+let text= `mitt namn är ${mynamn} Jag är ${myage}`
+
+console.log(text)
+
+let student= true
+console.log(student)
